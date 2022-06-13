5c3cc94766220ac51303f80d38664e1483787a76
Steven-Otieno <steveotianga8991@gmail.com>
2022-06-14 02:22:02 +0300

    commit tree 8b9edd2b7b0cc5dda083fc5d377d1abf3c96fb65

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#text').textContent = "This is really cool!";
+});
+
+document.querySelector('#text').textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
